1e4de9fa4f850e3871f8700ea52d2b0123b490b1
Gabe Kopley <gabe@coshx.com>
2012-09-04 13:24:08 -0700

    Record new episodes, fixes test

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,6 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :fakeweb
+  c.default_cassette_options = { :record => :new_episodes }
 end
 
 RSpec.configure do |config|
